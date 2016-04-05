5b084cf5df16a4bc7cf2eb9bf8d38fdb173ae9fb
Jordan DE GEA <jordan.degea@gmail.com>
2016-04-05 23:37:26 +0200

    Partie DAO

--- webproject/src/main/java/dao/PartieDAO.java
+++ webproject/src/main/java/dao/PartieDAO.java
@@ -5,10 +5,159 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import model.AbstractBaseModel;
+import model.PartieModel;
+
 /**
  *
  * @author JordanLeMagnifique
  */
-public class PartieDAO {
-    
+public class PartieDAO extends AbstractDataBaseDAO{
+    final private static PartieDAO instanceUnique = new PartieDAO();
+
+    static PartieDAO instance() {
+        return instanceUnique;
+    }
+
+    private PartieDAO(/*DataSource ds*/) {
+        super(/*ds*/);
+    }
+
+    @Override
+    public AbstractBaseModel get(int id) throws DAOException {
+        PartieModel result = null;
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Partie WHERE idPartie='" + id + "'");
+            if (rs.next()) {
+                result = new PartieModel(
+                        id,
+                        rs.getString("titrePartie"), 
+                        rs.getString("resumePartie"), 
+                        rs.getDate("date"), 
+                        rs.getString("lieu"), 
+                        rs.getBoolean("termine"));
+            }
+        } catch (SQLException e) {
+            throw new DAOException("DBError " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+        return result;
+    }
+
+    @Override
+    public List<AbstractBaseModel> getAll() throws DAOException {
+        List<AbstractBaseModel> result = new ArrayList<>();
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM Partie");
+            while (rs.next()) {
+                PartieModel ouvrage
+                     = new PartieModel(
+                        rs.getInt("idPartie"),
+                        rs.getString("titrePartie"), 
+                        rs.getString("resumePartie"), 
+                        rs.getDate("date"), 
+                        rs.getString("lieu"), 
+                        rs.getBoolean("termine"));
+                result.add(ouvrage);
+            }
+        } catch (SQLException e) {
+            throw new DAOException("DBError " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+        return result;
+    }
+
+    @Override
+    public int insert(Object object) throws DAOException {
+        if (!(object instanceof PartieModel)) {
+            throw new DAOException("Wrong object parameter in insert, require PartieModel");
+        }
+        int affectedRows = 0;
+        PartieModel partie = (PartieModel) object;
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            PreparedStatement st
+                    = conn.prepareStatement("INSERT INTO Partie (titrePartie ,resumePartie, datePartie, lieu, termine, idUnivers, loginMJ) VALUES (?,?,?)");
+            st.setString(1, partie.getTitrePartie());
+            st.setString(2, partie.getRésumé());
+            st.setDate(3, partie.getDate());
+            st.setString(4, partie.getLieu());
+            st.setBoolean(5, partie.isPartieFinie());
+            st.setInt(6, partie.getUnivers().getId());
+            st.setInt(7, partie.getMJ().getId());
+            affectedRows = st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("DBError " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+        return affectedRows ;
+    }
+
+    @Override
+    public int update(Object object) throws DAOException {
+        if (!(object instanceof PartieModel)) {
+            throw new DAOException("Wrong object parameter in update, require PartieModel");
+        }
+        int affectedRows = 0 ; 
+        PartieModel partie = (PartieModel) object;
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            PreparedStatement st
+                    = conn.prepareStatement("UPDATE Partie SET titrePartie=?, resumePartie=?, datePartie=?, lieu=?, termine=?, idUnivers=?, loginMJ=? WHERE idPartie=?");
+            st.setString(1, partie.getTitrePartie());
+            st.setString(2, partie.getRésumé());
+            st.setDate(3, partie.getDate());
+            st.setString(4, partie.getLieu());
+            st.setBoolean(5, partie.isPartieFinie());
+            st.setInt(6, partie.getUnivers().getId());
+            st.setInt(7, partie.getMJ().getId());
+            st.setInt(8, partie.getId());
+            affectedRows = st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("DBError " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+        return affectedRows ;
+    }
+
+    @Override
+    public int delete(Object object) throws DAOException {
+        if (!(object instanceof PartieModel)) {
+            throw new DAOException("Wrong object parameter in delete, require PartieModel");
+        }
+        int affectedRows = 0;
+        PartieModel partie = (PartieModel) object;
+        Connection conn = null;
+        try {
+            conn = getConnection();
+            PreparedStatement st
+                    = conn.prepareStatement("DELETE FROM Partie WHERE idPartie=?");
+            st.setInt(1, partie.getId());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DAOException("DBError " + e.getMessage(), e);
+        } finally {
+            closeConnection(conn);
+        }
+        return affectedRows ;
+    }
 }
